6a92d9b5bf5ffd9478712258f94811ce429fe4db
Ron Dekker <redkenrok@gmail.com>
2018-08-04 12:58:40 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# hoast-layout
-Hoast module used to transform the content of files using layouts.
+# Hoast-layout
+Transform the content of files using layouts.
+
+> As the name suggest this is a [Hoast](https://github.com/hoast/hoast#readme) module. The module has been based of [metalsmith-layouts](https://github.com/metalsmith/metalsmith-layouts#readme).
+
+## Usage
+
+### Parameters
+
+* `directory` **{{String}}**: The directory containing the layouts.
+	* Default: `''` (root of the source directory).
+* `layout` **{String}**: Path to default layout if the [frontmatter](https://github.com/hoast/hoast-frontmatter#readme) of the file does not specify another layout using an options named `layout`.
+	* Required: `yes`
+* `options` **{{Object}}**: Options given to the [JSTransformer](https://github.com/jstransformers/jstransformer#readme).
+	* Default: `{}`
+* `patterns` **{Array of strings}**: An array of string which gets used to match files using glob patterns. See [nanomatch](https://github.com/micromatch/nanomatch#readme) for more details on the patterns.
+	* Default: `[]`
+
+### Example
+
+**Cli**
+
+```json
+{
+  "modules": {
+    "read": {},
+    "hoast-layout": {
+      "directory": "layouts",
+      "layout": "page.hbs",
+      "patterns": [
+	    "**/*.html"
+      ]
+	}
+  }
+}
+```
+
+**Script**
+
+```javascript
+const Hoast = require('hoast');
+const read = Hoast.read,
+      layout = require('hoast-layout');
+
+Hoast(__dirname)
+  .use(read())
+  .use(layout({
+    directory: 'layouts'
+    layout: 'page.hbs',
+    patterns: [
+      '**/*.html'
+    ]
+  }))
+  .process();
+```
+
+> In the examples the HTML files are entered into the handlebars layout.
+
+## Troubleshooting
+
+If you are having problems with the module please [enable debugging](https://github.com/hoast/hoast#debugging) to have a closer look.
+
+**File not valid for processing.**
+* The file is not utf-8 encoded.
+* The file path does not match any of the patterns.
+
+**No valid transformer found for extension <extension>.**
+* Check if the JSTransformer associated with the extension is installed.
+
+## License
+[ISC license](https://github.com/hoast/hoast-layout/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,105 @@
+// Node modules.
+const assert = require('assert'),
+	  { join } = require('path');
+// Dependency modules.
+const jstransformer = require('jstransformer'),
+	  nanomatch = require('nanomatch'),
+	  totransformer = require('inputformat-to-jstransformer');
+// If debug available require it.
+let debug; try { debug = require('debug')('hoast-layout'); } catch(error) { debug = function() {}; }
+
+// Cached transformers.
+const transformers = {};
+
+/**
+ * Matches the extension to the transformer.
+ * @param {String} extension The layouts extension.
+ */
+const getTransformer = function(extension) {
+	// If transformer already cached return that.
+	if (extension in transformers) {
+		return transformers[clear];
+	}
+	
+	// Retrieve the transformer if available.
+	const transformer = totransformer(extension);
+	transformers[extension] = transformer ? jstransformer(transformer) : false;
+	
+	// Return transformer.
+	return transformers[extension];
+};
+
+const validateOptions = function(options) {
+	assert(typeof(options) === 'object', 'hoast.layout: options must be of type object.');
+	if (options.directory) {
+		assert(typeof(options.directory) === 'string', 'hoast-layout: directory must be of type string.');
+	}
+	assert(typeof(options.layout) === 'string', 'hoast-layout: layout is a required parameter and must be of type string.');
+	if (options.options) {
+		assert(typeof(options.options) === 'object', 'hoast-layout: options must be of type object.');
+	}
+	if (options.patterns) {
+		assert(Array.isArray(options.patterns)  && options.patterns.length, 'hoast-layout: patterns needs must be specified and an array of strings.');
+	}
+};
+
+/**
+ * Transforms files content using layouts.
+ * @param {Object} options The module options.
+ */
+module.exports = function(options) {
+	debug(`Initializing module.`);
+	
+	validateOptions(options);
+	debug(`Validated options.`);
+	options = Object.assign({
+		directory: '',
+		options: {},
+		patterns: []
+	}, options);
+	
+	return async function(hoast, files) {
+		debug(`Running module.`);
+		await Promise.all(
+			// Loop through files.
+			files.map(function(file) {
+				return new Promise(function(resolve) {
+					debug(`Processing file: '${file.path}'.`);
+					
+					assert(file.content !== null, 'hoast-layout: No content found on file, read module needs to be called before this.');
+					// Has to be a string and patterns if specified.
+					if (file.content.type !== 'string' || (options.patterns.length > 0 && !nanomatch.any(file.path, options.patterns))) {
+						debug(`File not valid for processing.`);
+						return resolve();
+					}
+					debug(`File content is valid.`);
+					
+					// Get layout from rontmatter.
+					let layout = options.layout;
+					if (file.frontmatter && file.frontmatter.layout) {
+						assert(typeof(file.frontmatter.layout) === 'string', 'hoast-layout: layout specified in frontmatter must be of type string.');
+					}
+					// Get layout extension.
+					layout = join(hoast.options.source, options.directory, layout);
+					debug(`Using layout '${layout}'.`);
+					
+					// Use given engine or retrieve transformer automatically.
+					const transformer = getTransformer(layout.split('.').pop());
+					if (!transformer) {
+						debug(`No valid transformer found for extension '${layout.split('.').pop()}'.`);
+						resolve();
+					}
+					
+					// Combine metadata and file data.
+					const data = Object.assign({}, hoast.options.metadata, file.frontmatter, { content: file.content.data });
+					
+					// Override content and extension.
+					file.content.data = transformer.renderFile(layout, options.options, data).body;
+					
+					debug(`Rendered file.`);
+					resolve();
+				})
+			})
+		);
+	};
+};
